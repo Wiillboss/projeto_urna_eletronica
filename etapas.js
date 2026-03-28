65d8af3f39f083341f72ed04991591a902bbf392
Willian dos Santos Silva <86166759+Wiillboss@users.noreply.github.com>
2026-03-27 21:40:53 -0300

    atualização

---
+++ etapas.js
@@ -0,0 +1,49 @@
+let etapas = [
+    {
+        titulo: 'Vereador',
+        numero: 5,
+        candidatos: [
+            {
+                numero: 38111,
+                nome: 'Fulando de Tal',
+                partido: 'ABC',
+                fotos: [
+                    { url: '38111.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: 77222,
+                nome: 'Beltrano da Silva',
+                partido: 'DEFG',
+                fotos: [
+                    { url: '77222.jpg', legenda: 'Vereador' }
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'Prefeito',
+        numero: 2,
+        candidatos: [
+            {
+                numero: 99,
+                nome: 'Ciclano',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos: [
+                    { url: '99.jpg', legenda: 'Prefeito' },
+                    { url: '99_2.jpg', legenda: 'Vice-Prefeito', small: true }
+                ]
+            },
+            {
+                numero: 84,
+                nome: 'Zulano',
+                partido: 'QWERTY',
+                fotos: [
+                    { url: '84.jpg', legenda: 'Prefeito' },
+                    { url: '84_2.jpg', legenda: 'Vice-Prefeito', small: true }
+                ]
+            },
+        ]
+    }
+];
